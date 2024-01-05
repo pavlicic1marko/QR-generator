2679a9115a7d51474ad5ec465899112c21b8448b
Marko Pavlicic <marko.pavlicic@computerrock.com>
2024-01-05 12:34:36 +0100

    adding QR creation logic

--- main.py
+++ main.py
@@ -6,6 +6,20 @@ from PIL import ImageTk, Image
 
 root = Tk()
 
+
+# on button click execute
+def generate():
+    link_name = name_entry.get()
+    link = link_entry.get()
+    file_name = link_name + ".png"
+    url = pyqrcode.create(link)
+    # create file with QR code
+    url.png(file_name, scale=8)
+    image = ImageTk.PhotoImage(Image.open(file_name))
+    image_label = Label(image=image)
+    image_label.image = image
+    canvas.create_window(200, 400, window=image_label)
+
 canvas = Canvas(root, width=400, height=600)
 canvas.pack()
 
@@ -25,7 +39,7 @@ canvas.create_window(200, 120, window=name_entry)
 canvas.create_window(200, 180, window=link_entry)
 
 # Add Button
-button = Button(text="Generate QR")
+button = Button(text="Generate QR", command=generate)
 canvas.create_window(200, 250, window=button)
 
 # start GUI
